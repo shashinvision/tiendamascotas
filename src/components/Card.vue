8344889d1e33e6a1b26462194a1750e94766331e
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-09 11:22:43 -0300

    [ADD] Front listo

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,11 +10,40 @@
       class="mb-2"
     >
       <b-card-text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+        <ul>
+          <li><b>Stock:</b> Disponible</li>
+          <li><b>Nombre Producto:</b> Lorem, ipsum dolor.</li>
+          <li><b>Precio Producto:</b> $10.000.</li>
+        </ul>
       </b-card-text>
 
-      <b-button href="#" variant="primary">Go somewhere</b-button>
+      <div class="flexBox">
+        <div class="def-number-input number-input border-danger">
+          <button
+            onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+            class="minus"
+          >
+            <b-iconstack font-scale="1">
+              <b-icon stacked icon="cart-dash" variant=""></b-icon>
+            </b-iconstack>
+          </button>
+          <input
+            class="quantity w-25"
+            min="0"
+            name="quantity"
+            value="1"
+            type="number"
+          />
+          <button
+            onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+            class="plus"
+          >
+            <b-iconstack font-scale="1">
+              <b-icon stacked icon="cart-plus" variant=""></b-icon>
+            </b-iconstack>
+          </button>
+        </div>
+      </div>
     </b-card>
   </div>
 </template>
@@ -23,4 +52,12 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+ul {
+  list-style-type: none;
+  margin-left: -30px;
+}
+.flexBox {
+  flex-direction: row-reverse;
+}
+</style>
