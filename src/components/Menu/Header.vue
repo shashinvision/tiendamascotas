9100a0941dbf004ab7051bf271321abd25ce5e0c
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-08 23:29:35 -0300

    [EDI] Space Menu

--- src/components/Menu/Header.vue
+++ src/components/Menu/Header.vue
@@ -5,23 +5,7 @@
         <a class="navbar-brand" href="#">Carousel</a>
 
         <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-                >Disabled</a
-              >
-            </li>
-          </ul>
+          <ul class="navbar-nav me-auto mb-2 mb-md-0"></ul>
           <form class="d-flex">
             <input
               class="form-control me-2"
